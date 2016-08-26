7a7b7a089041b8b88716c125b2bd50b0992a672f
jamestothe <jamesdelacruz@gmail.com>
2016-08-25 18:10:15 -0700

    updated schema pw to pw_digest uncommented bcrypt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160826002551) do
+ActiveRecord::Schema.define(version: 20160826010441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 20160826002551) do
     t.string   "last_name"
     t.string   "email"
     t.string   "user_name"
-    t.string   "password"
     t.string   "image_url"
     t.string   "course_taken"
     t.string   "city"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
