eca9df2edddeeb1a95e2b3db392d81914c62452f
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-08-26 13:37:46 -0700

    add CRUD to articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,27 @@
 class ArticlesController < ApplicationController
+
+  def show
+    @article = Article.find(params[:article_id])
+  end
+
+  def edit
+    @article = Article.find(params[:article_id])
+  end
+
+  def update
+    @article = Article.find(params[:article_id])
+    @article.update(article_params)
+  end
+
+  def destroy
+    @article = Post.find(params[:post_id])
+    @article.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:article).permit(:title, :content, :city, :article_type)
+  end
 end
