fa0bbc5fdf8320ad4977a0ceef38a489cfee107e
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-08-29 16:49:19 -0700

    fix end error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,11 +44,10 @@ include AuthHelper
         render :edit
       end
     end
-
+  end
 private
 
 def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :user_name, :course_taken, :city, :password)
 end
-
 end
