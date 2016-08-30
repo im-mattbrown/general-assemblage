949d6374d23240a76167faf7da9eed3c01701e97
Paolo Sese <jpaolo.sese@gmail.com>
2016-08-30 19:09:06 +0000

    added validations to articles controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
 
   before_action :logged_in
+  before_action :find_article, except: [:index]
+  before_action :find_comment, only: [:edit, :update, :destroy]
 
   def index
     @comments = Comment.all
@@ -10,11 +12,9 @@ class CommentsController < ApplicationController
     @user = current_user
     @comment = Comment.new
     @comment.user_id = @user.id
-    @article = Article.find_by_id(params[:article_id])
   end
 
   def create
-    @article = Article.find_by_id(params[:article_id])
     new_comment = Comment.new(comment_params)
     user_id = current_user[:id]
     new_comment[:user_id] = user_id
@@ -25,27 +25,30 @@ class CommentsController < ApplicationController
   end
 
   def edit
-    @comment = Comment.find_by_id(params[:id])
-    @article = Article.find_by_id(params[:article_id])
   end
 
   def update
-    @comment = Comment.find_by_id(params[:id])
-    @article = Article.find_by_id(params[:article_id])
     @comment.update(comment_params)
     redirect_to show_article_path(@article)
   end
   
   def destroy
-    @comment = Comment.find_by_id(params[:id])
-    @article = Article.find_by_id(params[:article_id])
     @comment.destroy
     redirect_to show_article_path
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:content)
-    end
+  
+  def find_comment
+    @comment = Comment.find_by_id(params[:id])
+  end
+  
+  def find_article
+    @article = Article.find_by_id(params[:article_id])
+  end
+  
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 
 end
